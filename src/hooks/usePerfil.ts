4750c1e003477db0432d62c26f7cc9322f151266
DanielitusV <virreira.daniel@gmail.com>
2026-04-06 22:43:46 -0400

    feat: Agregar hook useGetProfile para obtener perfil existente

--- src/hooks/usePerfil.ts
+++ src/hooks/usePerfil.ts
@@ -1,8 +1,18 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import { useNavigate } from '@tanstack/react-router'
 import api from '../api/axios'
 import type { ApiError } from '../interfaces/api.interface'
 import type { RegisterAccountDto } from '../dtos/user.dto'
+
+export const useGetProfile = () => {
+  return useQuery<RegisterAccountDto, ApiError>({
+    queryKey: ['profile'],
+    queryFn: async () => {
+      const res = await api.get('/profile')
+      return res.data
+    },
+  })
+}
 
 export const useCreateProfile = () => {
   const navigate = useNavigate()
