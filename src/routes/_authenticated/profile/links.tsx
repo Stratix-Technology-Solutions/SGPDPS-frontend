c9e14f3d3dc076794c65da513fbd818418c73f43
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2026-04-20 02:05:46 -0400

    fix: ajustar el titulo al estandar definido

--- src/routes/_authenticated/profile/links.tsx
+++ src/routes/_authenticated/profile/links.tsx
@@ -1,20 +1,18 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { LinkSection } from '../../../features/links/components/LinkSection'
+import { SectionTitle } from '../../../shared/components/SectionTitle'
 
 export const Route = createFileRoute('/_authenticated/profile/links')({
   component: RouteComponent,
 })
 
-
 function RouteComponent() {
   return (
     <div className="py-10 flex flex-col gap-6">
-      <div className="pl-4">
-        <h1 className="text-2xl font-bold text-background-dark">Enlaces públicos</h1>
-        <p className="text-sm text-neutral-medium/70">
-          Gestiona los enlaces que aparecerán en tu portafolio.
-        </p>
-      </div>
+      <SectionTitle
+        title="Enlaces públicos"
+        description="Gestiona los enlaces que aparecerán en tu portafolio."
+      />
 
       <LinkSection />
     </div>
