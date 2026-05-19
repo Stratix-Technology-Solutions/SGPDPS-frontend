00598939c2db9482ac3656389c40f6e5c59a982f
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2026-05-19 08:54:46 -0400

    feat: agregar boton de redireccion

--- src/routes/_authenticated/profile/visibility-settings.tsx
+++ src/routes/_authenticated/profile/visibility-settings.tsx
@@ -1,6 +1,7 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
 import { SectionTitle } from '../../../shared/components/SectionTitle'
 import { PortfolioSections } from '../../../features/view/components/PortfolioSections'
+import { useGetProfile } from '../../../features/profile/hooks/useGetProfile'
 
 export const Route = createFileRoute(
   '/_authenticated/profile/visibility-settings',
@@ -9,12 +10,23 @@ export const Route = createFileRoute(
 })
 
 function RouteComponent() {
+  const { data } = useGetProfile()
+
   return (
     <div className="py-10 flex flex-col gap-6">
       <SectionTitle
         title="Configuración de visibilidad"
         description="Elige que secciones de tu portafolio seran visibles para los visitantes."
       />
+
+      {!!data && (
+        <Link
+          to={`/profile/${(data as any).username}`}
+          className="w-full bg-primary text-white flex justify-center items-center py-3 rounded-xl hover:cursor-pointer hover:bg-primary-soft transition-all duration-300"
+        >
+          Ver perfil
+        </Link>
+      )}
 
       <PortfolioSections />
     </div>
