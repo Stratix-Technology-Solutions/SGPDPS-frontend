cebf13e60171cc0ca90c1143d883d837dd15ca91
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2026-05-22 13:21:21 -0400

    fix: corregir visualizacion del navbar al buscar perfiles

--- src/features/profiles/components/Navbar.tsx
+++ src/features/profiles/components/Navbar.tsx
@@ -1,28 +1,53 @@
 import { Link } from '@tanstack/react-router'
+import { useQuery } from '@tanstack/react-query'
+import { validateToken } from '../../auth/services/auth.service'
 
 export const Navbar = () => {
+  const { data: isAuthenticated } = useQuery({
+    queryKey: ['validate-token'],
+    queryFn: validateToken,
+    staleTime: 1000 * 60 * 5,
+  })
+
   return (
     <nav className="w-full border-b border-neutral-200 bg-white sticky top-0 z-20">
-      <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
-        <Link to="/" className="text-2xl font-extrabold tracking-wide text-primary">
-          FOLIO<span className="text-primary-soft">X</span>
+      <div className="max-w-7xl mx-auto min-h-20 px-6 flex py-4 items-center justify-between flex-wrap gap-2">
+        <Link
+          to={isAuthenticated ? '/dashboard' : '/'}
+          className="flex items-center gap-3"
+        >
+          <img
+            src="/logo.svg"
+            alt="logo FolioX"
+            className="w-11 md:w-12"
+          />
+
+          <span className="text-2xl md:text-3xl font-semibold leading-none tracking-widest text-background-dark">
+            FOLIO
+            <span className="text-primary text-3xl md:text-4xl">
+              X
+            </span>
+          </span>
         </Link>
 
-        <div className="flex items-center gap-4">
-          <Link
-            to="/login"
-            className="text-sm font-medium text-background-dark hover:text-primary-soft transition-colors"
-          >
-            Iniciar Sesión
-          </Link>
-          <Link
-            to="/register"
-            className="group relative overflow-hidden bg-primary-soft text-white px-4 py-2 rounded-lg text-sm font-semibold shadow-sm shadow-primary-soft/20 transition-all duration-200 hover:brightness-110 hover:shadow-md hover:shadow-primary-soft/30 active:scale-[0.98]"
-          >
-            <span className="absolute inset-0 -translate-x-full group-hover:translate-x-full transition-transform duration-500 bg-linear-to-r from-transparent via-white/20 to-transparent skew-x-12" />
-            Registrate
-          </Link>
-        </div>
+        {!isAuthenticated && (
+          <div className="flex items-center gap-3">
+
+            <Link
+              to="/login"
+              className="text-sm font-medium text-background-dark hover:text-primary-soft transition-colors"
+            >
+              Iniciar Sesión
+            </Link>
+
+            <Link
+              to="/register"
+              className="bg-primary-soft hover:brightness-110 text-white px-4 py-2 rounded-lg text-sm font-semibold transition-all duration-200"
+            >
+              Registrate
+            </Link>
+          </div>
+        )}
       </div>
     </nav>
   )
